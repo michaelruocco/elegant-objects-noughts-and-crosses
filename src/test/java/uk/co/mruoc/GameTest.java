2f0d6e0e989cb89d346208c6634f4a9f9b3edd23
Michael Ruocco <michael.ruocco@hotmail.com>
2025-06-20 10:19:29 +0100

    removes public modifier

--- src/test/java/uk/co/mruoc/GameTest.java
+++ src/test/java/uk/co/mruoc/GameTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class GameTest {
+class GameTest {
 
     private final Board board = new DefaultBoard().initialized();
     private final Players players = new Players();
