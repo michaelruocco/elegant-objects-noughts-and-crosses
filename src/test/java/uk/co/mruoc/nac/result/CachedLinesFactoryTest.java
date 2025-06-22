045299b15455a11b3622ab41592bf4371541e38c
Michael Ruocco <michael.ruocco@hotmail.com>
2025-06-22 13:28:04 +0100

    adds coverage to cached lines factory

---
+++ src/test/java/uk/co/mruoc/nac/result/CachedLinesFactoryTest.java
@@ -0,0 +1,18 @@
+package uk.co.mruoc.nac.result;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class CachedLinesFactoryTest {
+
+    @Test
+    void shouldOnlyCreateLinesOnFirstCall() {
+        var factory = new CachedLinesFactory(3);
+        var lines1 = factory.build();
+
+        var lines2 = factory.build();
+
+        assertThat(lines1).isEqualTo(lines2);
+    }
+}
